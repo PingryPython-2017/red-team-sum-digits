be0a1aea9341cf7a66f974ed3acef9e15b06fbd1
Matt Lee <mlee2020@pingry.org>
2017-05-02 12:26:22 -0400

    Fixed the code to make it simpler

--- sumDigits.py
+++ sumDigits.py
@@ -1,12 +1,8 @@
 def sumDigits(number):
 	""" Returns the sum of each digit in number """
-	
-	word = str(number)
-	digit = int(word[0])
-	newNumber = int(word[1:])
-	
-	if len(word) == 0: # Base case
+	if number == 0: # Base case
 		return 0
-	digit + sumDigits(newNumber)
+	else:
+		return (number % 10) + sumDigits(number // 10)
 
 print(sumDigits(999))
